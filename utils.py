83d465a46f6538db1ce39637765a38a81f7ba78c
francoisnation <francois@wearenation.co.uk>
2019-07-08 08:52:17 -0500

    Add comments to ead/modeling notebooks

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 """Utilities librairies."""
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def print_metrics(labels, preds):
@@ -9,6 +12,23 @@ def print_metrics(labels, preds):
     print("Accuracy Score: {}".format(accuracy_score(labels, preds)))
     print("F1 Score: {}".format(f1_score(labels, preds)))
     print("ROC_AUC_Score: {}".format(roc_auc_score(labels, preds)))
+
+
+def print_confusion_matrix(y_true, y_pred, class_names=[0, 1]):
+    cnf_matrix = confusion_matrix(y_true, y_pred)
+
+    fig, ax = plt.subplots()
+    tick_marks = np.arange(len(class_names))
+
+    # create heatmap
+    sns.heatmap(cnf_matrix, annot=True, cmap="YlGnBu", fmt='g')
+    ax.xaxis.set_label_position("top")
+    plt.tight_layout()
+    plt.title('Confusion matrix', y=1.1)
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+    plt.xticks(tick_marks + .5, class_names)
+    plt.yticks(tick_marks + .5, class_names)
 
 
 def find_best_k(X_train, y_train, X_test, y_test, min_k=1, max_k=25):
